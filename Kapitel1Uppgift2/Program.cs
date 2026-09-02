102e00d60833fb74b7fe4112292ff8e1ab2d6afd
kebo09001 <kebo09001@utb.vaxjo.se>
2026-09-02 10:00:05 +0200

    Add project files.

---
+++ Kapitel1Uppgift2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Kapitel1Uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("Ett problem har uppstått på datorn. Den måste startas om.");
+            Console.Write("Först kommer felinformation att samlas in och sedan utförs omstart automatiskt.");
+        }
+    }
+}
